1882f1aa479a34f48357f3087155d824882da5b9
lkj <932684335@qq.com>
2019-05-23 23:27:38 +0800

    4.2、完成header组件及数据渲染

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,9 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 0,/* 函数左括号 */
+    'indent': 0/* 最外层括号缩进不检验 */
   },
   parserOptions: {
     parser: 'babel-eslint'
